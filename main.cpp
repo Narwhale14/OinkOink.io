b3d0513a40f0f86d96668de0a12c84b2b8f4dce5
Niall Murray <npmurray@nic.edu>
2024-10-22 13:57:55 -0700

    Subtract

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 
 int add(int a, int b);
+int subtract(int a, int b);
 
 int main() {
     std::cout << "Hello world!\n";
     std::cout <<"Hello from another World!\n";
     std::cout << "Sum of 1 and 2 is not " << add(1, 3) << std::endl;
+    std::cout << "1 minus 2 is not " << subtract(1, 2) << std::endl;
 }
 
 int add(int a, int b) {
     return a + b;
+}
+
+int subtract(int a, int b) {
+    return b - a;
 }
