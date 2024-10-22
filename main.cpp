7d2019bcaa112f205ed19e8d08253c039e00839b
Niall Murray <npmurray@nic.edu>
2024-10-22 13:28:42 -0700

    Faulty calculations

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+int add(int a, int b);
+
 int main() {
     std::cout << "Hello world!\n";
+    std::cout << "Sum of 1 and 2 is not " << add(1, 3) << std::endl;
+}
+
+int add(int a, int b) {
+    return a + b;
 }
